fb1200906d3b10ba8d37811c3fe022cc4ba2677c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-13 15:14:43 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbilinehalfarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbilinehalfarcview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbilinehalfarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.bilinhalfarcview.BiLineHalfArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiLineHalfArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
