714b943aa32318e03e01896be4a24b8fb378c576
Anwesh43 <anweshthecool0@gmail.com>
2019-07-13 15:06:58 +0530

    using Renderer in View

--- bilinhalfarcview/src/main/java/com/anwesh/uiprojects/bilinhalfarcview/BiLineHalfArcView.kt
+++ bilinhalfarcview/src/main/java/com/anwesh/uiprojects/bilinhalfarcview/BiLineHalfArcView.kt
@@ -68,15 +68,16 @@ fun Canvas.drawBLHANode(i : Int, scale : Float, paint : Paint) {
 class BiLineHalfArcView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.draw(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
