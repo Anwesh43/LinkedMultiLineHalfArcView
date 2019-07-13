7b54e70e0fa2da5de614787a0d973ee280a33ce2
Anwesh43 <anweshthecool0@gmail.com>
2019-07-13 14:55:44 +0530

    adding BLHANode class

--- bilinhalfarcview/src/main/java/com/anwesh/uiprojects/bilinhalfarcview/BiLineHalfArcView.kt
+++ bilinhalfarcview/src/main/java/com/anwesh/uiprojects/bilinhalfarcview/BiLineHalfArcView.kt
@@ -127,4 +127,48 @@ class BiLineHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLHANode(var i : Int, val state : State = State()) {
+
+        private var next : BLHANode? = null
+        private var prev : BLHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLHANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLHANode {
+            var curr : BLHANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
